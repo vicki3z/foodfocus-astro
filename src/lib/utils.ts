7986556ea9e4c1e3401c1f423f51ff0502e2b8f8
vicki3z <victoria.pong@gmail.com>
2026-01-26 14:33:38 +1100

    fix(base-url): simplify baseUrl to be at the lowest level components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,7 @@ export function getBaseUrl(): string {
   const baseUrl = import.meta.env.BASE_URL;
   return baseUrl.endsWith('/') ? baseUrl : `${baseUrl}/`;
 }
+
+export function getPlaceHolderImageSrc(): string {
+  return `${getBaseUrl()}images/placeholder.png`;
+}
